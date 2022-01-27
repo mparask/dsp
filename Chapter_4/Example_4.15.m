592ffedd464c743e93ac34407f07e3136e13f25f
Michael Paraskevas <michael.paraskevas@gmail.com>
2022-01-27 16:42:50 +0200

    Delete Chapter_4 directory

--- Chapter_4/Example_4.15.m
+++
@@ -1,6 +0,0 @@
-% Example 4.15
-
-syms t s 
-X(s) = 1/(s+2); 
-x(t) = ilaplace(X(s), t) 
-ezplot(x(t)); grid on; xlim([0 4]); ylim([0 1.2])
--- Chapter_4/Example_4.30.m
+++
@@ -1,8 +0,0 @@
-% Example 4.30
-
-syms t s 
-u(t) = heaviside(t);
-h(t) = exp(-2*t) * ( u(t) - u(t-2) );
-H(s) = laplace(h(t), s)
-pretty(H(s))
-
