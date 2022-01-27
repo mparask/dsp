b98c4bd5b0a3ad75991df84e4fd8438961ab59da
Michael Paraskevas <michael.paraskevas@gmail.com>
2022-01-27 16:43:23 +0200

    Delete Chapter_8 directory

--- Chapter_8/Example_8.18.m
+++
@@ -1,5 +0,0 @@
-% Example 8.18
-
-b = [1, -2, 0, 1]
-a = [1, 2, -1]
-[R,p,C] = residuez(b,a)
--- Chapter_8/Example_8.31.m
+++
@@ -1,6 +0,0 @@
-% Example 8.31
-
-% FIR: y[n] = x[n] + 2x[n-1] - 0.5x[n-4]
-a = 1;  b = [1, 0, 2, 0, -0.5]
-[sos, g] = tf2sos(b,a);
-display(g); display(sos);
