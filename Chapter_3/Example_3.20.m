0563aedf0232de3e9ceeb697b0597cefdbbac6b8
Michael Paraskevas <michael.paraskevas@gmail.com>
2022-01-27 16:42:43 +0200

    Delete Chapter_3 directory

--- Chapter_3/Example_3.20.m
+++
@@ -1,9 +0,0 @@
-% Example 3.20
-clc; clear all; clf
-
-syms w t
-u(t) = heaviside(t);
-x(t) = exp(-2*t)*sin(4*t) * u(t);
-X(w) = fourier(x(t), w);
-subplot(211); ezplot(abs(X(w)), [-10,10]); grid on
-subplot(212); ezplot(angle(X(w)), [-10,10]); grid on
