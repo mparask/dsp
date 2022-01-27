7744c8caf4387c9bc34bec7cad373d9dc8d370b9
Michael Paraskevas <michael.paraskevas@gmail.com>
2022-01-27 16:42:18 +0200

    Delete Chapter_11 directory

--- Chapter_11/imp_resp.m
+++
@@ -1,7 +0,0 @@
-% Υλοποίηση κρουστικής απόκρισης του φίλτρου στο Matlab
-function [hd, n] = imp_resp(wc,N) % wc συχνότητα αποκοπής
-a = (N-1)/2;
-m = [0:1:(N-1)];
-n = m - a + eps;
-hd = sin(wc*n)./(pi*n);
-end
