7a54748f4f5cc76e81a5234a49004da1fd38ce35
Michael Paraskevas <michael.paraskevas@gmail.com>
2022-01-27 16:52:18 +0200

    Delete Chapter_12 directory

--- Chapter_12/Example_12.23.m
+++
@@ -1,5 +0,0 @@
-% Example 12.23
-
-syms x; y = exp(-x); ezplot(y); grid on
-
-ezplot(y,-2,2); grid on
--- Chapter_12/Example_12.26.m
+++
@@ -1,6 +0,0 @@
-% Example 12.26
-
-ts = 0.01;
-t = 0 : ts : 2;
-x = cos(5*pi*t) - sin(7*pi*t);
-plot(t, x); grid on
